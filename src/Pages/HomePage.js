e7ef7f32a56fa53c711fbe4ce105627f6478e316
EliasRoro <eliassaychelem@gmail.com>
2024-09-06 00:57:09 -0400

    Home Page Added

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Trusted Hands Care</h1>
+      <p>Overview of services, mission, and unique value proposition.</p>
+    </div>
+  );
+};
+
+export default HomePage;
