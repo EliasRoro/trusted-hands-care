f7969d13ef372e9cf705dd4d687e5413752d7b6a
EliasRoro <eliassaychelem@gmail.com>
2024-09-06 01:12:03 -0400

    Home page and app js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutUs from './components/AboutUs';
+import Services from './components/Services';
+import Caregivers from './components/Caregivers';
+import HowItWorks from './components/HowItWorks';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import FAQs from './components/FAQs';
+import Blog from './components/Blog';
+import ContactUs from './components/ContactUs';
+import Careers from './components/Careers';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import TermsOfService from './components/TermsOfService';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about-us" component={AboutUs} />
+        <Route path="/services" component={Services} />
+        <Route path="/caregivers" component={Caregivers} />
+        <Route path="/how-it-works" component={HowItWorks} />
+        <Route path="/pricing" component={Pricing} />
+        <Route path="/testimonials" component={Testimonials} />
+        <Route path="/faqs" component={FAQs} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/contact-us" component={ContactUs} />
+        <Route path="/careers" component={Careers} />
+        <Route path="/privacy-policy" component={PrivacyPolicy} />
+        <Route path="/terms-of-service" component={TermsOfService} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
